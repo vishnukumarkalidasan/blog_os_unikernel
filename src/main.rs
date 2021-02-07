d284e4830601bfdbe36ea4d9b76ae45ef20d4ab5
Philipp Oppermann <dev@phil-opp.com>
2021-02-07 12:12:03 +0100

    Add the `no_main` attribute

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-fn main() {}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
 
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
